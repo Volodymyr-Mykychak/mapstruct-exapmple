fc8de85f81edeeddbe5724a4c38e591cf795f8ed
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-11-22 22:16:47 +0200

    we create our own class EmployeeMapperImpl

--- src/main/java/mate/academy/mapstructexapmple/mapper/EmployeeMapper.java
+++ src/main/java/mate/academy/mapstructexapmple/mapper/EmployeeMapper.java
@@ -1,12 +1,9 @@
 package mate.academy.mapstructexapmple.mapper;
 
-import mate.academy.mapstructexapmple.config.MapperConfig;
 import mate.academy.mapstructexapmple.dto.CreateEmployeeRequestDto;
 import mate.academy.mapstructexapmple.dto.EmployeeDto;
 import mate.academy.mapstructexapmple.model.Employee;
-import org.mapstruct.Mapper;
 
-@Mapper(config = MapperConfig.class)
 public interface EmployeeMapper {
     EmployeeDto toDto(Employee employee);
 
---
+++ src/main/java/mate/academy/mapstructexapmple/mapper/EmployeeMapperImpl.java
@@ -0,0 +1,30 @@
+package mate.academy.mapstructexapmple.mapper;
+
+import mate.academy.mapstructexapmple.dto.CreateEmployeeRequestDto;
+import mate.academy.mapstructexapmple.dto.EmployeeDto;
+import mate.academy.mapstructexapmple.model.Employee;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeMapperImpl implements EmployeeMapper {
+    public EmployeeDto toDto(Employee employee) {
+        if (employee == null) {
+            return null;
+        }
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setId(employee.getId());
+        employeeDto.setName(employee.getName());
+        employeeDto.setEmail(employee.getEmail());
+        return employeeDto;
+    }
+
+    public Employee toModel(CreateEmployeeRequestDto requestDto) {
+        if (requestDto == null) {
+            return null;
+        }
+        Employee employee = new Employee();
+        employee.setName(requestDto.name());
+        employee.setEmail(requestDto.email());
+        return employee;
+    }
+}
